0cb6fc3d1d2299d42b736466f421f339d1e881e6
elsa-demaine <31849703+elsa-demaine@users.noreply.github.com>
2026-04-10 15:03:01 +0200

    First Upload

---
+++ deploy-commands.js
@@ -0,0 +1,31 @@
+﻿const { REST, Routes, SlashCommandBuilder, ChannelType } = require('discord.js');
+
+// 🔧 CONFIG
+const TOKEN = 'MYTOKEN;
+const CLIENT_ID = 'MyCLIENTID'; // from Developer Portal
+const GUILD_ID = '974030257432719381';  // your test server
+
+// Define commands
+const commands = [
+    new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Lists all of Wooly Helpers commands')
+].map(cmd => cmd.toJSON());
+
+// Register
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+    try {
+        console.log('Registering slash commands...');
+
+        await rest.put(
+            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+            { body: commands }
+        );
+
+        console.log('✅ Commands registered!');
+    } catch (error) {
+        console.error(error);
+    }
+})();
---
+++ index.js
@@ -0,0 +1,39 @@
+﻿const { Client, GatewayIntentBits } = require('discord.js');
+const memberController = require('./MemberController.js');
+const counting = require('./Counting.js');
+const channelController = require('./ChannelController.js');
+
+
+// === BOT SETUP ===
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,           // Access to guilds
+        GatewayIntentBits.GuildMembers,     // Access to members
+        GatewayIntentBits.GuildMessages,    // Access to messages
+        GatewayIntentBits.MessageContent    // Access to message content
+    ],
+    partials: ['GUILD_MEMBER','MESSAGE', 'CHANNEL', 'REACTION'] // helps with update events
+});
+
+// === READY EVENT ===
+client.once('clientReady', async () => {
+    console.log(`Bot online: ${client.user.tag}`);
+    //const guild = client.guilds.cache.first();
+    //await guild.members.fetch();
+    client.user.setActivity('Wool Growing Competition');
+});
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'help') {
+        await interaction.reply('Wooly Helper has /help commands');
+    }
+});
+
+// Pass client to controller
+memberController.init(client);
+counting.init(client);
+channelController.init(client);
+
+client.login('TOKEN');
